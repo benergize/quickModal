a312e76db283cd7950ba963267acc7883d9e89b0
Benjamin Ehrlich <bensaysstuff@gmail.com>
2019-11-07 09:00:00 -0500

    Create quickModal.js

---
+++ quickModal.js
@@ -0,0 +1,38 @@
+function quickModal(message) {
+			let modal = document.createElement("div");
+			modal.style.position = 'fixed';
+			modal.style.zIndex = 99999;
+			modal.style.left = "0";
+			modal.style.top = "0";
+			modal.style.width="100%";
+			modal.style.height="100%";
+			modal.style.background="rgba(0,0,0,.55)";
+			modal.style.display = "flex";
+			modal.style.alignItems = "center";
+			modal.style.justifyContent = "center";
+			modal.id = "modalmodalmodal";
+			modal.style.transition = "all .3s";
+			modal.style.opacity = 0;
+			
+			let modalContent = document.createElement("div");
+			modalContent.style.padding = "10px";
+			modalContent.style.background = "white";
+			modal.appendChild(modalContent); 
+			
+			let close = document.createElement("a");
+			close.style.display = "block";
+			close.style.float = "right";
+			close.style.color = "red";
+			close.innerHTML = "&times;";
+			close.onclick = function(){this.parentElement.parentElement.remove();}
+			close.href = "javascript:void(0)";
+			modalContent.appendChild(close);
+			
+			let content = document.createElement("div");
+			content.innerHTML = message;
+			content.style.padding = "24px";
+			modalContent.appendChild(content);
+			
+			document.body.appendChild(modal);
+			modal.style.opacity = 1;
+		}

44acf4ae63908a5978357462a7a139c4e2a6da61
Benjamin Ehrlich <bensaysstuff@gmail.com>
2020-06-01 14:58:38 -0400

    Update quickModal.js
    Added support for closing by clickout, arguments by object, and on-close type function.

--- quickModal.js
+++ quickModal.js
@@ -1,7 +1,24 @@
-function quickModal(message, showX = true, animate=true) {
+function quickModal(message, showX = true, animate=true, closeAction = function() {}) {
+	
+	if(typeof message === "object") {
+		
+		if(typeof message.message != "undefined") {
+		
+			if(typeof message.showX !== "undefined") { showX = message.showX; }
+			if(typeof message.animate !== "undefined") { animate = message.animate; }
+			if(typeof message.closeAction !== "undefined") { closeAction = message.closeAction; } 
+			message = message.message; 
+		}
+	}
+	
 
 	//Remove a previously created potentially still active modal
-	if(document.getElementById('modalmodalmodal') !== null) { document.getElementById('modalmodalmodal').remove(); }
+	if(document.getElementById('modalmodalmodal') !== null) { 
+
+		document.getElementById('modalmodalmodal').remove(); 
+	}
+	
+	if(message === false) { return true; } 
 
 	//Create the container
 	let modal = document.createElement("div");
@@ -18,6 +35,9 @@ function quickModal(message, showX = true, animate=true) {
 	modal.id = "modalmodalmodal";
 	modal.style.transition = "all .3s";
 	modal.style.opacity = 0;
+	modal.onmousedown = function(ev) {
+		if(ev.path[0] === this) { closeAction(); document.querySelector("#modalmodalmodal").remove(); }
+	}
 
 	//Create the inner modal window
 	let modalContent = document.createElement("div");
@@ -36,21 +56,23 @@ function quickModal(message, showX = true, animate=true) {
 		close.style.color = "red";
 		close.style.textDecoration = "none";
 		close.innerHTML = "&times;";
-		close.onclick = function(){this.parentElement.parentElement.remove();}
+		close.onclick = function(){ closeAction(); this.parentElement.parentElement.remove(); }
 		close.href = "javascript:void(0)";
 		modalContent.appendChild(close);
 	}
 
 	//Create and populate inner modal content
 	let content = document.createElement("div");
-	content.innerHTML = message;
 	content.style.padding = "24px";
+	
+	if(typeof message === "string") { content.innerHTML = message; }
+	else if(typeof message === "object") { content.appendChild(message); }
 	modalContent.appendChild(content);
 
 	//Add to DOM
 	document.body.appendChild(modal);
 	
 	//Show the modal either with a fadein or all at once
-	if(animate) { setTimeout(d=>{modal.style.opacity = 1;},100); }
+	if(animate) { setTimeout(d=>{ modal.style.opacity = 1;}, 100); }
 	else { modal.style.opacity = 1; }
 }
